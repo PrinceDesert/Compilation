fde7702abad3fde8838d26f21f27fda3f3dd35e8
DesertPrince <lepetitprince10@gmx.fr>
2022-04-09 17:10:21 +0200

    Update

--- TP4/inc/synthesized_type_expression.h
+++ TP4/inc/synthesized_type_expression.h
@@ -2,6 +2,10 @@
 #define _TYPESYNTH_H
 // expression de type : (le type synthétisé pour les expressions)
 // ce type est presque similaire à symbol_type mais il sert surtout pour ERROR_TYPE
-typedef enum synthesized_type_expression { T_INT, T_BOOLEAN, AND_T, OR_T, GT_T, LT_T, ERROR_TYPE} synthesized_type_expression;
-
+typedef enum synthesized type_expression { T_INT, T_BOOLEAN, AND_T, OR_T, GT_T, LT_T, ERROR_TYPE} synthesized type_expression;
+/**
+ * GT_T : >
+ * LT_T : <
+ * ERROR_TYPE : 1+true
+*/
 #endif
